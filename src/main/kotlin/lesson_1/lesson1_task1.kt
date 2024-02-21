cb57ea9f87cd70b4d5cc748ddbd527a4dd872fa2
zharnikov <volodiazharnikov@yandex.ru>
2024-02-21 18:06:30 +0300

    KS-1-1
    solution to the first task

--- src/main/kotlin/lesson_1/lesson1_task1.kt
+++ src/main/kotlin/lesson_1/lesson1_task1.kt
@@ -3,7 +3,6 @@ package lesson_1
 fun main () {
 
     val numberOfOrders: Int = 75
-
     val thanksForOrder: String = "Спасибо за то, что выбрали наш магазин! В ближайщее время с вами свяжется менеджер для уточнения деталей заказа! "
 
 }
