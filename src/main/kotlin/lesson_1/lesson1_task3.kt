a4e18062a7a6e4f49ed0ec012835d0b8895c1c64
zharnikov <volodiazharnikov@yandex.ru>
2024-02-21 17:02:15 +0300

    KS-1-3
    solution to the third task

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,18 @@
+package lesson_1
+
+    fun main() {
+
+        val year = 1961
+
+        var hour = 9
+        var minute = 7
+
+        println(year)
+        println(hour)
+        println(minute)
+
+        hour = 10
+        minute = 55
+        println("$hour:$minute")
+
+    }
