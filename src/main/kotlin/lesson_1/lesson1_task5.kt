53986f2cedd3ac7e12728dac5a689e57b1d2ccb4
zharnikov <volodiazharnikov@yandex.ru>
2024-02-21 17:39:58 +0300

    KS-1-5
    solution to the fifth task

---
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -0,0 +1,13 @@
+package lesson_1
+
+fun main() {
+
+    var timeInTheSpace = 6480
+
+    timeInTheSpace = 48
+
+    val hoursInTheSpace = 1
+
+    println("0$hoursInTheSpace:$timeInTheSpace:00")
+
+}
