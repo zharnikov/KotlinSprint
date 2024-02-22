1db504dd1123685aecb633a8e5c020acb36749da
zharnikov <volodiazharnikov@yandex.ru>
2024-02-22 23:49:46 +0300

    KS-2-5
    решение 5 задачи из 2 урока

---
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -0,0 +1,14 @@
+package lesson_2
+
+import kotlin.math.pow
+
+fun main() {
+
+    val contribution = 70000
+    val interestRate = 16.7
+    val depositTerm = 20
+    val totalSum = (contribution * (1 + (interestRate/100)).pow(depositTerm))
+
+    println( String.format("%.3f", totalSum))
+
+}
