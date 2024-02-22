b4f0c3d7a91ef5dc558118252aa6196f662cf4dd
zharnikov <volodiazharnikov@yandex.ru>
2024-02-22 18:53:55 +0300

    KS-1-3
    solution to the third task

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -3,16 +3,15 @@ package lesson_1
     fun main() {
 
         val year = 1961
-
-        var hour = 9
-        var minute = 7
+        var hour: String = "09"
+        var minute: String = "07"
 
         println(year)
         println(hour)
         println(minute)
 
-        hour = 10
-        minute = 55
+        hour = "10"
+        minute = "55"
         println("$hour:$minute")
 
     }
