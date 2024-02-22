c4f3e39ef56643c3096da9a0b25ef441dab101df
zharnikov <volodiazharnikov@yandex.ru>
2024-02-22 22:00:37 +0300

    KS-2-2
    solution to the second task

---
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -0,0 +1,18 @@
+package lesson_2
+
+fun main() {
+
+    val workers = 50
+    val workersSalary = 30000
+    val trainees = 30
+    val traineesSalary = 20000
+
+    val totalWorkersSalary = workers * workersSalary
+    val totalSalary = totalWorkersSalary + (trainees * traineesSalary)
+    val averageSalary = totalSalary/(workers + trainees)
+
+    println("Зарплата постоянных сотрудников: $totalWorkersSalary")
+    println("Общая сумма расходов на зарплату: $totalSalary")
+    println("Средняя зарплата сотрудника: $averageSalary")
+
+}
