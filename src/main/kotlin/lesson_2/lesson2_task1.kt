a83a74f5e88e528cdc54acd42836f6c6266fbdb3
zharnikov <volodiazharnikov@yandex.ru>
2024-02-22 19:54:21 +0300

    KS-2-1
    solution to the first task

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,13 @@
+package lesson_2
+
+fun main() {
+
+    val firstStudent: Float = 3.0f
+    val secondStudent: Float = 4.0f
+    val thirdStudent: Float = 3.0f
+    val fourthStudent: Float = 5.0f
+    val averageMark = (firstStudent + secondStudent + thirdStudent + fourthStudent)/4
+
+    println(averageMark)
+
+}
