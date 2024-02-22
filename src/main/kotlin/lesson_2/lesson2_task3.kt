777881b19cc8d580cb656dfcfed968a48dc2b88b
zharnikov <volodiazharnikov@yandex.ru>
2024-02-23 00:33:42 +0300

    KS-2-3
    решение 3 задачи из 2 урока

---
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -0,0 +1,14 @@
+package lesson_2
+
+fun main() {
+
+    val timeStartHour = 9
+    val timeStartMinute = 39
+    val timeInTravel = 457
+
+    val timeFinishHour = ((timeStartHour * 60 + timeInTravel + timeStartMinute) / 60 ) % 24
+    val timeFinishMinute = (timeStartMinute + timeInTravel) % 60
+
+    println(String.format("Время прибытия поезда: %02d:%02d", timeFinishHour, timeFinishMinute))
+
+}
