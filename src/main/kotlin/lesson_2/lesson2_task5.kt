9415a16ed40b3511620ee0cba52df1cfc3f32efd
zharnikov <volodiazharnikov@yandex.ru>
2024-02-23 07:58:01 +0300

    KS-2-5
    воспользовался автоформатированием кода

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -7,8 +7,8 @@ fun main() {
     val contribution = 70000
     val interestRate = 16.7
     val depositTerm = 20
-    val totalSum = (contribution * (1 + (interestRate/100)).pow(depositTerm))
+    val totalSum = (contribution * (1 + (interestRate / 100)).pow(depositTerm))
 
-    println( String.format("%.3f", totalSum))
+    println(String.format("%.3f", totalSum))
 
 }
