99f7981333d84695abb5505572a171d655f962fd
zharnikov <volodiazharnikov@yandex.ru>
2024-02-23 08:03:24 +0300

    KS-1-3
    воспользовался автоформатированием кода

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,17 +1,17 @@
 package lesson_1
 
-    fun main() {
+fun main() {
 
-        val year = 1961
-        var hour: String = "09"
-        var minute: String = "07"
+    val year = 1961
+    var hour: String = "09"
+    var minute: String = "07"
 
-        println(year)
-        println(hour)
-        println(minute)
+    println(year)
+    println(hour)
+    println(minute)
 
-        hour = "10"
-        minute = "55"
-        println("$hour:$minute")
+    hour = "10"
+    minute = "55"
+    println("$hour:$minute")
 
-    }
+}
