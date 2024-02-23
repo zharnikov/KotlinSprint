d13f32505ae4e3a6795ce8d2dadd4f74a6697721
zharnikov <volodiazharnikov@yandex.ru>
2024-02-23 08:02:39 +0300

    KS-2-5
    воспользовался автоформатированием кода

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -2,11 +2,11 @@ package lesson_1
 
 fun main() {
 
-    val secondsInTheSpace= 6480
+    val secondsInTheSpace = 6480
     val hourInTheSpace = secondsInTheSpace / 3600
     val minuteInTheSpace = (secondsInTheSpace % 3600) / 60
     val secondInTheSpace = secondsInTheSpace % 60
 
-    println( String.format("%02d:%02d:%02d", hourInTheSpace, minuteInTheSpace, secondInTheSpace) )
+    println(String.format("%02d:%02d:%02d", hourInTheSpace, minuteInTheSpace, secondInTheSpace))
 
 }
