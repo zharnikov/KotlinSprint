e4c1e18f0729e03630b67af8c38d9885d2f8ea36
zharnikov <volodiazharnikov@yandex.ru>
2024-02-23 07:59:27 +0300

    KS-2-3
    воспользовался автоформатированием кода

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -6,7 +6,7 @@ fun main() {
     val timeStartMinute = 39
     val timeInTravel = 457
 
-    val timeFinishHour = ((timeStartHour * 60 + timeInTravel + timeStartMinute) / 60 ) % 24
+    val timeFinishHour = ((timeStartHour * 60 + timeInTravel + timeStartMinute) / 60) % 24
     val timeFinishMinute = (timeStartMinute + timeInTravel) % 60
 
     println(String.format("Время прибытия поезда: %02d:%02d ", timeFinishHour, timeFinishMinute))
