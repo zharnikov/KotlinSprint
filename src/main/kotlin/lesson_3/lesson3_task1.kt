5f09f360e4ac5a21a471c17836ad9ccc76c79085
zharnikov <volodiazharnikov@yandex.ru>
2024-02-24 10:26:34 +0300

    KS-3-1
    решение первой задачи 3 урока

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,12 @@
+package lesson_3
+
+fun main() {
+
+    val nameOfUser = "Владимир"
+    var greetings = "Добрый день, "
+    println("$greetings$nameOfUser")
+
+    greetings = "Добрый вечер, "
+    println("$greetings$nameOfUser")
+
+}
