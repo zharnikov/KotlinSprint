e6ef2c6958102e8235d2b26d724532a9f520c160
zharnikov <volodiazharnikov@yandex.ru>
2024-02-24 11:24:52 +0300

    K-3-4
    решение 4 задачи 3 урока

---
+++ src/main/kotlin/lesson_3/lesson3-task4.kt
@@ -0,0 +1,15 @@
+package lesson_3
+
+fun main() {
+
+    var moveFrom = "E2"
+    var moveTo = "E4"
+    var numberOfMove = "1"
+    println("$moveFrom-$moveTo;$numberOfMove")
+
+    moveFrom = "D2"
+    moveTo = "D3"
+    numberOfMove = "2"
+    println("$moveFrom-$moveTo;$numberOfMove")
+
+}
