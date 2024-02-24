85646033f4ae888a24a5f741255b5e5d575fc6a1
zharnikov <volodiazharnikov@yandex.ru>
2024-02-24 09:46:21 +0300

    KS-1-5
    добавил const, чтобы избавиться от магических чисел

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,12 +1,17 @@
 package lesson_1
 
+const val minutesInHour = 60
+const val secondsInMinute = 60
+
 fun main() {
 
     val secondsInTheSpace = 6480
-    val hourInTheSpace = secondsInTheSpace / 3600
-    val minuteInTheSpace = (secondsInTheSpace % 3600) / 60
-    val secondInTheSpace = secondsInTheSpace % 60
+    val hourInTheSpace = secondsInTheSpace / (minutesInHour * secondsInMinute)
+    val minuteInTheSpace = (secondsInTheSpace % (minutesInHour * secondsInMinute)) / secondsInMinute
+    val secondInTheSpace = secondsInTheSpace % secondsInMinute
 
     println(String.format("%02d:%02d:%02d", hourInTheSpace, minuteInTheSpace, secondInTheSpace))
+
+    println((secondsInTheSpace % 3600))
 
 }
