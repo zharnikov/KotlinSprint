8d6ec00da22e23a77c058522e3e208383d66837e
zharnikov <volodiazharnikov@yandex.ru>
2024-02-24 10:37:11 +0300

    K-3-2
    решение 2 задачи 3 урока

---
+++ src/main/kotlin/lesson_3/lesson3-_task2.kt
@@ -0,0 +1,15 @@
+package lesson_3
+
+fun main() {
+
+    var surname = "Андреева"
+    val name = "Татьяна"
+    val patronymic = "Сергеевна"
+    var yearsOld = 20
+    println("$surname $name $patronymic" + ", " + yearsOld)
+
+    surname = "Сидорова"
+    yearsOld = 22
+    println("$surname $name $patronymic" + ", " + yearsOld)
+
+}
