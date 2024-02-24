606701d7b391a2eb3fe3b04b64d4aa96e66adebf
zharnikov <volodiazharnikov@yandex.ru>
2024-02-24 09:40:57 +0300

    KS-2-3
    добавил const, чтобы избавиться от магических чисел

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,14 +1,20 @@
 package lesson_2
 
+const val hoursInDay = 24
+const val minutesInHour = 60
+
+
 fun main() {
 
     val timeStartHour = 9
     val timeStartMinute = 39
     val timeInTravel = 457
 
-    val timeFinishHour = ((timeStartHour * 60 + timeInTravel + timeStartMinute) / 60) % 24
-    val timeFinishMinute = (timeStartMinute + timeInTravel) % 60
+    val timeFinishHour = ((timeStartHour * minutesInHour + timeInTravel + timeStartMinute) / minutesInHour) % hoursInDay
+    val timeFinishMinute = (timeStartMinute + timeInTravel) % minutesInHour
 
     println(String.format("Время прибытия поезда: %02d:%02d ", timeFinishHour, timeFinishMinute))
+
+    println((timeStartMinute + timeInTravel))
 
 }
