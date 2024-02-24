ec832c86538ff2767abde986d515fa9445f20364
zharnikov <volodiazharnikov@yandex.ru>
2024-02-24 11:12:57 +0300

    K-3-3
    решение 3 задачи 3 урока

---
+++ src/main/kotlin/lesson_3/lesson3-task3.kt
@@ -0,0 +1,21 @@
+package lesson_3
+
+fun main() {
+
+    val a = 7
+
+    println(
+        """
+        $a * 1 = ${a * 1}
+        $a * 2 = ${a * 2}
+        $a * 3 = ${a * 3}
+        $a * 4 = ${a * 4}
+        $a * 5 = ${a * 5}
+        $a * 6 = ${a * 6}
+        $a * 7 = ${a * 7}
+        $a * 8 = ${a * 8}
+        $a * 9 = ${a * 9}      
+    """.trimIndent()
+    )
+
+}
