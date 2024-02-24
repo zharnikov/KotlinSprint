05646959c9acef634364357c05facc99dfcdd35c
zharnikov <volodiazharnikov@yandex.ru>
2024-02-24 15:07:00 +0300

    K-1-5
    - изменил орфографию нейминга констант
    - удалил лишнии строки

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,17 +1,15 @@
 package lesson_1
 
-const val minutesInHour = 60
-const val secondsInMinute = 60
+const val MINUTESINHOR = 60
+const val SECONDSINMINUTE = 60
 
 fun main() {
 
     val secondsInTheSpace = 6480
-    val hourInTheSpace = secondsInTheSpace / (minutesInHour * secondsInMinute)
-    val minuteInTheSpace = (secondsInTheSpace % (minutesInHour * secondsInMinute)) / secondsInMinute
-    val secondInTheSpace = secondsInTheSpace % secondsInMinute
+    val hourInTheSpace = secondsInTheSpace / (MINUTESINHOR * SECONDSINMINUTE)
+    val minuteInTheSpace = (secondsInTheSpace % (MINUTESINHOR * SECONDSINMINUTE)) / SECONDSINMINUTE
+    val secondInTheSpace = secondsInTheSpace % SECONDSINMINUTE
 
     println(String.format("%02d:%02d:%02d", hourInTheSpace, minuteInTheSpace, secondInTheSpace))
-
-    println((secondsInTheSpace % 3600))
 
 }
