6a05c125781601b1dcc0050f47b5accdb20bf4ec
zharnikov <volodiazharnikov@yandex.ru>
2024-02-24 15:41:07 +0300

    KS-4-1
    решение 1 задачи из 4 урока

--- src/main/kotlin/lesson_4/lesson4-task1.kt
+++ src/main/kotlin/lesson_4/lesson4-task1.kt
@@ -1,14 +1,14 @@
 package lesson_4
 
-const val NUMBEROFTABLES = 13
+const val NUMBER_OF_TABLES = 13
 
 fun main() {
 
     val reservedTableForToday = 13
     val reservedTableForTomorrow = 9
     println(
-        "Доступность столиков на сегодня: ${reservedTableForToday < NUMBEROFTABLES} \n" +
-                "Доступность столиков на завтра: ${reservedTableForTomorrow < NUMBEROFTABLES}"
+        "Доступность столиков на сегодня: ${reservedTableForToday < NUMBER_OF_TABLES} \n" +
+                "Доступность столиков на завтра: ${reservedTableForTomorrow < NUMBER_OF_TABLES}"
     )
 
 }
