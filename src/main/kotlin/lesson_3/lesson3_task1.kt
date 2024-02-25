ea495adbd83b65f17073b55154fc01b32dc1811e
zharnikov <volodiazharnikov@yandex.ru>
2024-02-25 09:55:09 +0300

    KS-3-1
    - убрал знаки препинания из переменной и добавил их в принт

--- src/main/kotlin/lesson_3/lesson3_task1.kt
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -3,10 +3,10 @@ package lesson_3
 fun main() {
 
     val nameOfUser = "Владимир"
-    var greetings = "Добрый день, "
-    println("$greetings$nameOfUser")
+    var greetings = "Добрый день"
+    println("$greetings, $nameOfUser")
 
-    greetings = "Добрый вечер, "
-    println("$greetings$nameOfUser")
+    greetings = "Добрый вечер"
+    println("$greetings, $nameOfUser")
 
 }
