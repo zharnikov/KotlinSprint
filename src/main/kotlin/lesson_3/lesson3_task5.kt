37a4a282a7d48618be690d0c2210ef1bbe60c45e
zharnikov <volodiazharnikov@yandex.ru>
2024-02-26 09:50:37 +0300

    K-3-5
    - сделал корректное название для переменной

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -3,10 +3,10 @@ package lesson_3
 fun main() {
 
     val move = "D2-D4;0"
-    val a = move.split("-", ";")
-    val moveFrom = a[0]
-    val moveTo = a[1]
-    val numberOfMove = a[2]
+    val moveValues = move.split("-", ";")
+    val moveFrom = moveValues[0]
+    val moveTo = moveValues[1]
+    val numberOfMove = moveValues[2]
 
     println(moveFrom)
     println(moveTo)
