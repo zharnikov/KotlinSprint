f4eb80de9a0e30985e57aa30e9e6da72088e1283
zharnikov <volodiazharnikov@yandex.ru>
2024-02-28 11:31:16 +0300

    K-4-3
    - сделал более релевантные названия для переменных и констант

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,20 +1,20 @@
 package lesson_4
 
-const val SUNNY_WEATHER = true
-const val OPEN_TENT = true
-const val AIR_HUMIDITY = 20
+const val IS_SUNNY = true
+const val IS_TENT_OPEN = true
+const val HUMIDITY = 20
 const val SEASON = "winter"
 
 fun main() {
 
-    val sunnyWeatherToday = true
-    val openTentToday = true
-    val airHumidityToday = 20
+    val isSunny = true
+    val isTentOpen = true
+    val humidityToday = 20
     val seasonToday = "winter"
 
     val goodConditions =
-        SUNNY_WEATHER == sunnyWeatherToday && OPEN_TENT == openTentToday &&
-                AIR_HUMIDITY == airHumidityToday && SEASON != seasonToday
+        IS_SUNNY == isSunny && IS_TENT_OPEN == isTentOpen &&
+                HUMIDITY == humidityToday && SEASON != seasonToday
 
     println("Благоприятные ли условия сейчас для роста бобовых? $goodConditions")
 
