c937939e87a022bd224c232cd8247e7757d5f67e
zharnikov <volodiazharnikov@yandex.ru>
2024-02-28 12:41:57 +0300

    K-5-1
    - решение 1 задачи из 5 урока

---
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -0,0 +1,11 @@
+package lesson_5
+
+const val SOLUTION = 30
+
+fun main() {
+
+    println("Какой результат вычисления будет у выражения: 28 + 2 ?")
+    val answer = readln().toInt()
+    if (answer == SOLUTION) println("Добро пожаловать!") else println("Доступ запрещен.")
+
+}
