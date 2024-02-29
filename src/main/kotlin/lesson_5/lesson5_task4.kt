b0f961799409dce630e1f5c90809474f6d64031e
zharnikov <volodiazharnikov@yandex.ru>
2024-02-29 21:48:53 +0300

    K-5-4
    - улучшил читабельность кода

--- src/main/kotlin/lesson_5/lesson5_task4.kt
+++ src/main/kotlin/lesson_5/lesson5_task4.kt
@@ -9,13 +9,14 @@ fun main() {
 
     println("Для входа в систему ведите имя пользователя:")
     val userName = readln()
+
     if (userName == USER_NAME) {
         println("Введите пароль:")
         val password = readln()
         if (password == PASSWORD) {
             println("Доступ разрешен!")
         } else println("Неверный пароль! В доступе отказано!")
-    } else {
+    } ячдelse {
         println(
             """
             Такого пользователя в базе нет, нужно зарегистрироваться.
