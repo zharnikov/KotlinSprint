2c351b068b6b2411f6f0f552b228079354671c61
zharnikov <volodiazharnikov@yandex.ru>
2024-02-29 08:45:04 +0300

    K-5-5
    - решение к 5 задаче из 5 урока

---
+++ src/main/kotlin/lesson_5/lesson5_ask5.kt
@@ -0,0 +1,36 @@
+package lesson_5
+
+import kotlin.random.Random
+
+fun main() {
+
+    val randomNumbers = List(3) { Random.nextInt(0, 42) }
+
+    println(
+        """
+        Здравствуйте! Для того, чтобы выиграть главный приз, Вы должны угадать три числа.
+        Они лежат в промежутке от 0 до 42 включительно и являются целыми. 
+        Если хотите испытать удачу, то вводите первое число:
+    """.trimIndent()
+    )
+    val firstNumber = readln().toInt()
+
+    println("Вводите второе число:")
+    val secondNumber = readln().toInt()
+
+    println("Вводите третье число:")
+    val thirdNumber = readln().toInt()
+
+    val usersNumbers = listOf(firstNumber, secondNumber, thirdNumber)
+    val coincidences = (randomNumbers intersect usersNumbers).size
+
+    println("Загаданные числа: ${randomNumbers.toString().replace("[", "").replace("]", "")}.")
+
+    when (coincidences) {
+        3 -> println("Вы угадали все три и выиграли главный приз!")
+        2 -> println("Вы угадали два и выиграли приз!")
+        1 -> println("Вы угадали одно и выиграли утишительный приз!")
+        else -> println("Вы не угадали, но не растраивайтесь, попробуйте ещё!")
+    }
+
+}
