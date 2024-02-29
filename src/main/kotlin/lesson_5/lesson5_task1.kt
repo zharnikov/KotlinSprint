ea5c6a060d74f70432e0ea0c8b7c7443f7b18792
zharnikov <volodiazharnikov@yandex.ru>
2024-02-29 21:36:48 +0300

    K-5-1
    - убрал лишнюю константу

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -2,12 +2,11 @@ package lesson_5
 
 const val FIRST_SOLUTION = 28
 const val SECOND_SOLUTION = 2
-const val SOLUTION = 30
 
 fun main() {
 
     println("Какой результат вычисления будет у выражения: $FIRST_SOLUTION + $SECOND_SOLUTION ?")
     val answer = readln().toInt()
-    if (answer == SOLUTION) println("Добро пожаловать!") else println("Доступ запрещен.")
+    if (answer == FIRST_SOLUTION + SECOND_SOLUTION) println("Добро пожаловать!") else println("Доступ запрещен.")
 
 }
