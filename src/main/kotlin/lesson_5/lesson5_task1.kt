a72cf30fd31d919d356a15ce470a8dde7066f067
zharnikov <volodiazharnikov@yandex.ru>
2024-02-29 05:54:22 +0300

    K-5-1
    - добавил константы для участников сложения

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -1,10 +1,12 @@
 package lesson_5
 
+const val FIRST_SOLUTION = 28
+const val SECOND_SOLUTION = 2
 const val SOLUTION = 30
 
 fun main() {
 
-    println("Какой результат вычисления будет у выражения: 28 + 2 ?")
+    println("Какой результат вычисления будет у выражения: $FIRST_SOLUTION + $SECOND_SOLUTION ?")
     val answer = readln().toInt()
     if (answer == SOLUTION) println("Добро пожаловать!") else println("Доступ запрещен.")
 
