a84bda7e34a540a40c4dcf4821f826d4d1ec8f3b
zharnikov <volodiazharnikov@yandex.ru>
2024-03-05 21:46:03 +0300

    K-6-4
    - убрал лишние элементы и пустые строки

--- src/main/kotlin/lesson_6/lesson6_task4.kt
+++ src/main/kotlin/lesson_6/lesson6_task4.kt
@@ -3,7 +3,6 @@ package lesson_6
 fun main() {
 
     val randomNumber = (1..9).random()
-    println(randomNumber)
     val attemptNumber = 5
 
     for (i in attemptNumber downTo 1) {
@@ -14,13 +13,10 @@ fun main() {
         if (userNumber == randomNumber) {
             println("Это была великолепная игра!")
             break
-        } else continue
+        }
 
     }
 
     println("Было загадано число $randomNumber!")
 
 }
-
-
-
