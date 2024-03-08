8d6544d67f6b7d45e6ef2c7996ffb682eb38c129
zharnikov <volodiazharnikov@yandex.ru>
2024-03-08 20:43:04 +0300

    K-8-4

---
+++ src/main/kotlin/lesson_8/lesson8_task4.kt
@@ -0,0 +1,22 @@
+package lesson_8
+
+fun main() {
+
+    var productsForLasagna = mutableListOf("макароны", "фарш", "сыр")
+    println("Ингридиенты для лазаньи: ${productsForLasagna.joinToString(", ")}.")
+
+    println("Укажите ингредиент, который хотите убрать:")
+    val userProductToDelete = readln()
+    val numOfDeletedProduct = productsForLasagna.indexOf(userProductToDelete)
+
+    if (productsForLasagna.contains(userProductToDelete)) {
+
+        println("Какой ингридиент вы хотите добавить?")
+        val userProductToAdd = readln()
+        productsForLasagna[numOfDeletedProduct] = userProductToAdd
+
+        println("Готово! Вы сохранили следующий список: ${productsForLasagna.joinToString(", ")}.")
+
+    } else println("Такого элемента нет в списке!")
+
+}
