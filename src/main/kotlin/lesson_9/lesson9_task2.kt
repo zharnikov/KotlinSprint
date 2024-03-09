148c6862b3fb44be97c99aa3e17e34633a858b4a
zharnikov <volodiazharnikov@yandex.ru>
2024-03-09 12:54:07 +0300

    K-9-2

---
+++ src/main/kotlin/lesson_9/lesson9_task2.kt
@@ -0,0 +1,23 @@
+package lesson_9
+
+const val DECISION_TO_ACCESS = "да"
+
+fun main() {
+
+    val listOfProducts = mutableListOf("макароны", "сыр", "фарш")
+    println("В рецепте есть базовые ингредиенты: ${listOfProducts.joinToString()}.")
+
+    println("Желаете добавить еще?")
+    val userDecision = readln()
+
+    if (userDecision == DECISION_TO_ACCESS) {
+
+        println("Какой ингредиент вы хотите добавить?")
+        val userProduct = readln()
+        listOfProducts.add(userProduct)
+
+        println("Теперь в рецепте есть следующие ингредиенты: ${listOfProducts.joinToString()}.")
+
+    }
+
+}
