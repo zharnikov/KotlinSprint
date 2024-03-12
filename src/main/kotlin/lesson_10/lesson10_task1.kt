306a056349c23c40a70489746127c13120892379
zharnikov <volodiazharnikov@yandex.ru>
2024-03-12 12:05:11 +0300

    K-10-1

--- src/main/kotlin/lesson_10/lesson10_task1.kt
+++ src/main/kotlin/lesson_10/lesson10_task1.kt
@@ -2,5 +2,22 @@ package lesson_10
 
 fun main() {
 
+    val humanRoll = rollOfTheDice()
+    println("Бросок человека, на костях выпало: $humanRoll")
+    val computerRoll = rollOfTheDice()
+    println("Бросок компьютера, на костях выпало: $computerRoll")
+
+    if (humanRoll > computerRoll) {
+        println("Победило человечество")
+    } else if (humanRoll < computerRoll) {
+        println("Победила машина")
+    } else println("Победила дружба")
+
+}
+
+fun rollOfTheDice(): Int {
+
+    val randomNumber = (1..6).random()
+    return randomNumber
 
 }
