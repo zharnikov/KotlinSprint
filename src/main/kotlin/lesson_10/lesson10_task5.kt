0b83695b84d1f8e3a306742da61ebdafebaeef65
zharnikov <volodiazharnikov@yandex.ru>
2024-03-14 16:16:04 +0300

    K-10-5

---
+++ src/main/kotlin/lesson_10/lesson10_task5.kt
@@ -0,0 +1,22 @@
+package lesson_10
+
+const val LOGIN = "Zharnikov"
+const val PASSWORD = "Vladimir38"
+val basket = arrayOf(
+    "Макароны",
+    "Фарш",
+    "Лук"
+)
+
+fun main() {
+
+    println("Ваш логин:")
+    val userLogin = readln()
+
+    println("Ваш пароль:")
+    val userPassword = readln()
+
+
+
+
+}
