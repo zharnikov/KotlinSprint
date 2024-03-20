637d9095df4f84c976d298ae66195db132a44d44
zharnikov <volodiazharnikov@yandex.ru>
2024-03-20 16:22:20 +0300

    K-12-4

---
+++ src/main/kotlin/lesson_11/lesson11_task1.kt
@@ -0,0 +1,36 @@
+package lesson_11
+
+fun main() {
+
+    val user1 = User(
+        id = 1,
+        login = "first",
+        password = "1",
+        mail = "first@1.ru"
+    )
+
+    val user2 = User(
+        id = 2,
+        login = "second",
+        password = "2",
+        mail = "second@2.ru"
+    )
+
+    user1.printUser()
+    user2.printUser()
+
+}
+
+
+class User(
+
+    val id: Int,
+    val login: String,
+    val password: String,
+    val mail: String,
+
+    ) {
+
+    fun printUser() = println("id: $id, login: $login, password: $password, mail: $mail")
+
+}
