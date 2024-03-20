bade2511d390a52c7dabb43a23ff6732cc652ae2
zharnikov <volodiazharnikov@yandex.ru>
2024-03-20 15:42:52 +0300

    K-12-3

---
+++ src/main/kotlin/lesson_12/lesson12_task3.kt
@@ -0,0 +1,40 @@
+package lesson_12
+
+fun main() {
+
+    val day = Temperature123(300, 500, false)
+    day.printValues()
+
+}
+
+const val CELSIUS = 273.15
+
+class Temperature123(
+
+    dayTemperature: Int,
+    nightTemperature: Int,
+    isPrecipitation: Boolean
+
+) {
+
+    private val dayTemperatureCelsius = (dayTemperature - CELSIUS).toInt()
+    private val nightTemperatureCelsius = (nightTemperature - CELSIUS).toInt()
+    private val isPrecipitationCelsius = isPrecipitation
+
+    fun printValues() {
+
+        println(
+            """
+        Дневная температура: $dayTemperatureCelsius
+        Ночная температура: $nightTemperatureCelsius
+        Наличие осадков в течении суток: $isPrecipitationCelsius        
+    """.trimIndent()
+        )
+
+    }
+
+}
+
+
+
+
