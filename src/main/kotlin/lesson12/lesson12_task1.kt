cf4de94ad08e545ee8835f05b832a4506b4a8c86
zharnikov <volodiazharnikov@yandex.ru>
2024-03-20 14:38:21 +0300

    K-12-1

---
+++ src/main/kotlin/lesson12/lesson12_task1.kt
@@ -0,0 +1,34 @@
+package lesson12
+
+fun main() {
+
+    val day1 = Temperature()
+    val day2 = Temperature()
+
+    day1.printValues(10, 5, false)
+    day1.printValues(-10, -25, true)
+
+}
+
+class Temperature() {
+
+    val dayTemperature = 0
+    val nightTemperature = 0
+    val isPrecipitation = false
+
+    fun printValues(
+        dayTemperature: Int,
+        nightTemperature: Int,
+        isPrecipitation: Boolean
+    ) {
+        println(
+            """
+        Дневная температура: $dayTemperature
+        Ночная температура: $nightTemperature
+        Наличие осадков в течении суток: $isPrecipitation        
+    """.trimIndent()
+        )
+
+    }
+
+}
